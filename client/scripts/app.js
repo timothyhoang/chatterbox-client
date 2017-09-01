d489610432cd3a3082cc205ccb66328c89007770
Timothy Hoang <timothy.hoang@berkeley.edu>
2017-09-01 15:14:26 -0700

    Implemented barebone functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,52 @@
 // YOUR CODE HERE:
+app = {
+  server: 'http://parse.sfm6.hackreactor.com/'
+};
+
+app.init = function() {
+
+};
+
+app.send = function(message) {
+  $.ajax({
+    type: 'POST',
+    url: app.server,
+    data: message,
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    type: "GET",
+    url: app.server
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.renderMessage = function(message) {
+  var $element = $(`<div class="chat">${message.text}</div>`);
+  $('#chats #${}').append($element);
+};
+
+app.renderRoom = function(roomName) {
+  var $element = $(`<option id="${roomName}">${roomName}</option>`);
+  $('#roomSelect').append($element);
+};
+
+app.handleUsernameClick = function() {
+
+};
+
+app.handleSubmit = function() {
+
+};
